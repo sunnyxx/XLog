c3b3a985064e5c1be4db62491f955c18e60a1dcf
Yuan.Sun <sunyuan1713@gmail.com>
2013-10-18 18:45:22 +0800

    update structure

--- xlog/xlog/src/SNSLogging.h
+++ xlog/xlog/src/SNSLogging.h
@@ -1,6 +1,6 @@
 //
 //  SNSLogging.h
-//  xlog
+//  XLogBase
 //
 //  Created by sunny on 13-10-17.
 //
@@ -9,41 +9,45 @@
 #import "XLog.h"
 
 // log
-#define SYLog(format...)    XLog(@"sy", XLogInfoLevel, format)
-#define CSLog(format...)    XLog(@"cs", XLogInfoLevel, format)
-#define WYQLog(format...)   XLog(@"wyq",XLogInfoLevel, format)
-#define ZZYLog(format...)   XLog(@"zzy",XLogInfoLevel, format)
-#define ZHLog(format...)    XLog(@"zh", XLogInfoLevel, format)
-#define TSCLog(format...)   XLog(@"tsc",XLogInfoLevel, format)
-#define LWLog(format...)    XLog(@"lw", XLogInfoLevel, format)
-#define PHMLog(format...)   XLog(@"phm",XLogInfoLevel, format)
-#define WLLog(format...)    XLog(@"wl", XLogInfoLevel, format)
+#define SYLog(format...)    XLogBase(@"sy", XInfoLevel, format)
+#define CSLog(format...)    XLogBase(@"cs", XInfoLevel, format)
+#define WYQLog(format...)   XLogBase(@"wyq",XInfoLevel, format)
+#define ZZYLog(format...)   XLogBase(@"zzy",XInfoLevel, format)
+#define ZHLog(format...)    XLogBase(@"zh", XInfoLevel, format)
+#define TSCLog(format...)   XLogBase(@"tsc",XInfoLevel, format)
+#define LWLog(format...)    XLogBase(@"lw", XInfoLevel, format)
+#define PHMLog(format...)   XLogBase(@"phm",XInfoLevel, format)
+#define WLLog(format...)    XLogBase(@"wl", XInfoLevel, format)
 // warning
-#define SYWarning(format...)    XLog(@"sy", XLogWarningLevel, format)
-#define CSWarning(format...)    XLog(@"cs", XLogWarningLevel, format)
-#define WYQWarning(format...)   XLog(@"wyq",XLogWarningLevel, format)
-#define ZZYWarning(format...)   XLog(@"zzy",XLogWarningLevel, format)
-#define ZHWarning(format...)    XLog(@"zh", XLogWarningLevel, format)
-#define TSCWarning(format...)   XLog(@"tsc",XLogWarningLevel, format)
-#define LWWarning(format...)    XLog(@"lw", XLogWarningLevel, format)
-#define PHMWarning(format...)   XLog(@"phm",XLogWarningLevel, format)
-#define WLWarning(format...)    XLog(@"wl", XLogWarningLevel, format)
+#define SYWarning(format...)    XLogBase(@"sy", XWarningLevel, format)
+#define CSWarning(format...)    XLogBase(@"cs", XWarningLevel, format)
+#define WYQWarning(format...)   XLogBase(@"wyq",XWarningLevel, format)
+#define ZZYWarning(format...)   XLogBase(@"zzy",XWarningLevel, format)
+#define ZHWarning(format...)    XLogBase(@"zh", XWarningLevel, format)
+#define TSCWarning(format...)   XLogBase(@"tsc",XWarningLevel, format)
+#define LWWarning(format...)    XLogBase(@"lw", XWarningLevel, format)
+#define PHMWarning(format...)   XLogBase(@"phm",XWarningLevel, format)
+#define WLWarning(format...)    XLogBase(@"wl", XWarningLevel, format)
 // error
-#define SYError(format...)    XLog(@"sy", XLogErrorLevel, format)
-#define CSError(format...)    XLog(@"cs", XLogErrorLevel, format)
-#define WYQError(format...)   XLog(@"wyq",XLogErrorLevel, format)
-#define ZZYError(format...)   XLog(@"zzy",XLogErrorLevel, format)
-#define ZHError(format...)    XLog(@"zh", XLogErrorLevel, format)
-#define TSCError(format...)   XLog(@"tsc",XLogErrorLevel, format)
-#define LWError(format...)    XLog(@"lw", XLogErrorLevel, format)
-#define PHMError(format...)   XLog(@"phm",XLogErrorLevel, format)
-#define WLError(format...)    XLog(@"wl", XLogErrorLevel, format)
+#define SYError(format...)    XLogBase(@"sy", XErrorLevel, format)
+#define CSError(format...)    XLogBase(@"cs", XErrorLevel, format)
+#define WYQError(format...)   XLogBase(@"wyq",XErrorLevel, format)
+#define ZZYError(format...)   XLogBase(@"zzy",XErrorLevel, format)
+#define ZHError(format...)    XLogBase(@"zh", XErrorLevel, format)
+#define TSCError(format...)   XLogBase(@"tsc",XErrorLevel, format)
+#define LWError(format...)    XLogBase(@"lw", XErrorLevel, format)
+#define PHMError(format...)   XLogBase(@"phm",XErrorLevel, format)
+#define WLError(format...)    XLogBase(@"wl", XErrorLevel, format)
 
 // modules
-#define DBLog(format...)        XLog(@"Database", XLogLevel, format)
-#define DBWarning(format...)    XLog(@"Database", XLogWarningLevel, format)
-#define DBError(format...)      XLog(@"Database", XLogErrorLevel, format)
-#define NetworkLog(format...)        XLog(@"Network", XLogLevel, format)
-#define NetworkWarning(format...)    XLog(@"Network", XLogWarningLevel, format)
-#define NetworkError(format...)      XLog(@"Network", XLogErrorLevel, format)
+#define DBLog(format...)        XLogBase(@"Database", XInfoLevel, format)
+#define DBWarning(format...)    XLogBase(@"Database", XWarningLevel, format)
+#define DBError(format...)      XLogBase(@"Database", XErrorLevel, format)
+#define NetworkLog(format...)        XLogBase(@"Network", XInfoLevel, format)
+#define NetworkWarning(format...)    XLogBase(@"Network", XWarningLevel, format)
+#define NetworkError(format...)      XLogBase(@"Network", XErrorLevel, format)
+
+@interface SNSLoggingDelegate : NSObject <XLoggerDelegate>
+
+@end
 
---
+++ xlog/xlog/src/SNSLogging.m
@@ -0,0 +1,68 @@
+//
+//  SNSLogging.m
+//  xlog
+//
+//  Created by Sunny on 13-10-18.
+//
+//
+
+#import "SNSLogging.h"
+
+@interface SNSLoggingDelegate ()
+
+@end
+
+@implementation SNSLoggingDelegate
+
+static SNSLoggingDelegate* singleton = nil;
+static NSDictionary* configDict = nil;
+
++ (void)load
+{
+    // delegate
+    singleton = [self new];
+    [XLogger defaultLogger].delegate = singleton;
+    
+    // plist
+    NSString* path = [[NSBundle mainBundle] pathForResource:@"SNSLogging.plist" ofType:nil];
+    configDict = [NSDictionary dictionaryWithContentsOfFile:path];
+    if (!configDict)
+    {
+        XLog(@"SNSLoggingDelegate: config plist(%@) not found", path);
+    }
+}
+
+#pragma mark - XLogger delegate
+
+- (BOOL)XLogger:(XLogger *)logger shouldShowOwner:(NSString *)owner level:(XLogLevel)level
+{
+    NSDictionary* showDict = configDict[@"Owner"];
+    XLogLevel showBits = [showDict[owner] unsignedIntegerValue];
+    XLogLevel allBits = [showDict[XLoggerOwnerKeyAll] unsignedIntegerValue];
+    return (showBits & level) || (allBits & level);
+}
+
+- (BOOL)XLogger:(XLogger *)logger shouldShowComponent:(NSString *)componentKey
+{
+    return [configDict[@"Setting"][componentKey] boolValue];
+}
+
+- (NSString *)XLogger:(XLogger *)logger titleForLevel:(XLogLevel)level
+{
+    if (level & XInfoLevel)
+    {
+        return configDict[@"Setting"][@"InfoTitle"];
+    }
+    else if (level & XWarningLevel)
+    {
+        return configDict[@"Setting"][@"WarningTitle"];
+    }
+    else if (level & XErrorLevel)
+    {
+        return configDict[@"Setting"][@"ErrorTitle"];
+    }
+    return nil;
+}
+
+
+@end
